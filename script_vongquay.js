6411bbaf7ae308e9ee9b4ca0e4ae54a297a0f4fc
hibrogame1234 <h85620974@gmail.com>
2026-04-02 23:11:54 +0700

    Update script_vongquay.js

--- script_vongquay.js
+++ script_vongquay.js
@@ -60,7 +60,7 @@ function initGame() {
         if (!historyBody) return;
         historyBody.innerHTML = "";
         if (data) {
-            const list = Object.values(data).reverse().slice(0, 30);
+            const list = Object.values(data).reverse();
             historyBody.innerHTML = list.map(i => `
                 <tr>
                     <td style="color: #888; font-size: 0.8rem;">${i.time}</td>
